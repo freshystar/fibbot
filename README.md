fa85a2d858d037e83f19a3623694ac2b2e0a90e5
freshystar <thondipiongange@gmail.com>
2025-02-27 11:08:09 +0100

    feat: input parsing

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ## Hello World docker action 
 
-This action prints "Hello World" to the `action.log`.
+This action prints "Hello World" to the `action.log`. Accepts 
 
 #### Example usage
 
 ```
 uses: actions/fibbot@v1
 ```
+
+## Parameter parsing
+This action equally receives and processes inputs from the Github worflow.
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-
+use std::env;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // if args.len() != 3 {
-    //     println!("Enter exactly two arguments.Invalid input!!!");
-    // } else {
-    //     let argument_1 = &args[1];
-    //     let argument_2 = &args[2];
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Enter exactly two arguments.Invalid input!!!");
+    } else {
+        let argument_1 = &args[1];
+        let argument_2 = &args[2];
         println!("Hello, world!");
-        // println!("{}", argument_1);
-        // println!("{}", argument_2);
-    
+        println!("{}", argument_1);
+        println!("{}", argument_2);
+    }
 }
